83743ac8c36d6b2a04f083c69ddf7baa3baac446
Renato <renatomarquesteles@gmail.com>
2019-07-11 00:15:59 -0300

    relacionamento de tarefas e usuarios

--- src/main/java/br/com/renatomarquesteles/gerenciadortarefas/controllers/TarefasController.java
+++ src/main/java/br/com/renatomarquesteles/gerenciadortarefas/controllers/TarefasController.java
@@ -2,6 +2,7 @@ package br.com.renatomarquesteles.gerenciadortarefas.controllers;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.renatomarquesteles.gerenciadortarefas.modelos.Tarefa;
+import br.com.renatomarquesteles.gerenciadortarefas.modelos.Usuario;
 import br.com.renatomarquesteles.gerenciadortarefas.repositorios.RepositorioTarefa;
+import br.com.renatomarquesteles.gerenciadortarefas.servicos.ServicoUsuario;
 
 @Controller
 @RequestMapping("/tarefas")
@@ -23,11 +26,15 @@ public class TarefasController {
 	@Autowired
 	private RepositorioTarefa repositorioTarefa;
 
+	@Autowired
+	private ServicoUsuario servicoUsuario;
+	
 	@GetMapping("/listar")
-	public ModelAndView listar() {
+	public ModelAndView listar(HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("tarefas/listar");
-		mv.addObject("tarefas", repositorioTarefa.findAll());
+		String emailUsuario = request.getUserPrincipal().getName();
+		mv.addObject("tarefas", repositorioTarefa.carregarTarefasPorUsuario(emailUsuario));
 		return mv;
 	}
 
@@ -39,7 +46,7 @@ public class TarefasController {
 	}
 
 	@PostMapping("/inserir")
-	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result, HttpServletRequest request) {
 		ModelAndView mv = new ModelAndView();
 		if (tarefa.getDataExpiracao() == null) {
 			result.rejectValue("dataExpiracao", "tarefa.dataExpiracaoInvalida",
@@ -54,8 +61,11 @@ public class TarefasController {
 			mv.setViewName("tarefas/inserir");
 			mv.addObject(tarefa);
 		} else {
-			mv.setViewName("redirect:/tarefas/listar");
+			String emailUsuario = request.getUserPrincipal().getName();
+			Usuario usuarioLogado = servicoUsuario.encontrarPorEmail(emailUsuario);
+			tarefa.setUsuario(usuarioLogado);
 			repositorioTarefa.save(tarefa);
+			mv.setViewName("redirect:/tarefas/listar");
 		}
 		return mv;
 	}
